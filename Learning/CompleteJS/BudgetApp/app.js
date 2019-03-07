1cdc40f0ca7ebb0ef9480721bafb11e6f9232ef6
slash-lk <30907245+slash-lk@users.noreply.github.com>
2019-03-07 23:29:49 +0530

    CJS-84

--- Learning/CompleteJS/BudgetApp/app.js
+++ Learning/CompleteJS/BudgetApp/app.js
@@ -105,6 +105,20 @@ var UIController = (function() {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
+
+        clearFields: function() {
+            var fields = document.querySelectorAll(DOMstrings.inputDescription + ',' + DOMstrings.inputValue);
+
+            // convert list to an array
+            var fieldArray = Array.prototype.slice.call(fields);
+
+            fieldArray.forEach(element => {
+                element.value = "";
+            });
+
+            fieldArray[0].focus();
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -143,9 +157,12 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 3. Add the item to the UI
         UICtrl.addListItem(newItem, input.type);
 
-        // 4. Calculate the budget
+        // 4. Clear the fields
+        UICtrl.clearFields();
 
-        // 5. Display the budget on the UI
+        // 5. Calculate the budget
+
+        // 6. Display the budget on the UI
     };
 
     return {
